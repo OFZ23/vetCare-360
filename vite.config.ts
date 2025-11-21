342c62176d0adbe49cc75e9d5c4d0639aff396cf
OF23-arch <oscar2211590@correo.uis.edu.co>
2025-11-21 09:59:59 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  //base: "/vetCare-360/",
+  base: "/vetCare-360/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    define: {
+    "import.meta.env.VITE_SUPABASE_URL": JSON.stringify("https://xueqqvtjflbyjlnpxwpg.supabase.co"),
+    "import.meta.env.VITE_SUPABASE_ANON_KEY": JSON.stringify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1ZXFxdnRqZmxieWpsbnB4d3BnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0MjA4MTAsImV4cCI6MjA3ODk5NjgxMH0.GlGEa55_ecf2lZGvSibf4ipoIbLYNeae29olNOZVLFc"),
+  },
   },
 }));
 
